32e6cb717677f5b26e0247856644e5436d5fcbfd
15902124763@163.com <15902124763@163.com>
2018-07-27 17:03:03 +0800

    修改启动main 添加注释

--- Spring-Boot-App/src/main/java/com/yarm/Application.java
+++ Spring-Boot-App/src/main/java/com/yarm/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 @SpringBootApplication
-@MapperScan("com.yarm.mybatis.mapper")
+@MapperScan("com.yarm.mybatis.mapper")//扫描mapper
 public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
